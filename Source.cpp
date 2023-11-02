b9e2b16931c8c5c3b19bec0b18955f3fa32428e3
Kittel Watne <269425@stud.inn.no>
2023-11-02 16:13:46 +0100

    doing the commenting which i forgot, hopefully nothing breaks...

--- Source.cpp
+++ Source.cpp
@@ -10,8 +10,12 @@
 
 
 int main(){
+	//creating the main folder to run the program
 	Folder Jumbo("hovedMappe");
+	//running the program
 	Jumbo.runProgram(0);
+	//top tier comedy
 	std::cout << "\nYou have escaped the Matrix!\n";
+
 	return 0;
 }
